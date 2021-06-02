339157ada5d3e9c93bf9fe7e8f5320598005f161
Yohannes Getu <nede.evoli@gmail.com>
2021-06-02 15:14:58 +0300

    added popup information dynamicaly

--- map.py
+++ map.py
@@ -4,15 +4,16 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lon = list(data["LON"])
 lat = list(data["LAT"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[38.58, -99.09],
                  zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
+for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.Marker(location=[lt, ln],
-                 popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+                 popup=str(el) + " m", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
